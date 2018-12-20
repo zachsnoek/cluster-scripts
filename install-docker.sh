20f324a85b66dacf35e2bc17911d4b30aaadf605
zachsnoek <zacharysnoek@hope.edu>
2018-12-20 14:17:19 -0500

    Initial commit

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,6 @@
 bexec yum install -y yum-utils device-mapper persistent-data lvm2
 bexec yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-bexec yum install -y docker-ce 
+bexec yum install -y docker-ce
 bexec systemctl start docker
 bexec systemctl enable docker
 bexec docker run hello-world
